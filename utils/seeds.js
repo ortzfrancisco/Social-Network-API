f909205cf8909ea3a8c1a37f6e16bec2214fb5c0
francisco ortiz <franciscoortz89@gmail.com>
2024-06-27 12:26:53 -0400

    update utils seed.js

---
+++ utils/seeds.js
@@ -0,0 +1,31 @@
+// Imports
+const { User, Thought, Reaction } = require("../models");
+const mongoose = require("mongoose");
+
+const connection = require("../config/connection");
+
+// Seed data
+const users = [
+    {
+        username: "Ryan",
+        email: "ryan@gmail.com",
+        thought: [],
+    },
+];
+
+console.log(connection);
+
+// Connects to server
+connection.once("open", async () => {
+    console.log("connected");
+
+    // Drop existing students
+    await User.deleteMany({});
+
+    // Adds seed data to database
+    await User.collection.insertMany(users);
+
+    console.table(users);
+    console.info("Seeding complete! 🌱");
+    process.exit(0);
+});
